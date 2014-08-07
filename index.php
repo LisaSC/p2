0939771500eeb7a246709d8637c6d1c084a29867
LisaSC <ink@mit.edu>
2014-08-07 18:43:42 -0400

    added text to website

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 
 <body>
 	<h1>Silicon Ball</h1>
+	<p>
+	You may have heard that a crystal ball has the power to show the future. Like <br>
+	many westerners, you may find such traditional forecasting outdated. This tool <br>
+	aims to remedy the situation by incorporating recent technological advancements <br>
+	such as uncapitalized and unpunctuated sentences into the traditional practice. <br>
+	Please select your preferred sentence length. Beginners may wish to select a <br>
+	shorter	length to start, while advanced students will relish the cryptic form<br>
+	of longer sentences.
+	</p>
 	<form action='index.php' method='get'>
 		<input type='text' pattern=[1-9] name='num_words' size=1 value=<?php echo empty($_GET) ? 3 : $_GET['num_words']?>> <!-- value uses ternery operator-->
 		(1-9)<br>
