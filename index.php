a0bbf4b4f633add04804e5ab0f070f9a821a3917
LisaSC <ink@mit.edu>
2014-08-06 23:53:43 -0400

    commented code

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
 <body>
 	<h1>Silicon Ball</h1>
 	<form action='index.php' method='get'>
-		<input type='text' pattern=[1-9] name='num_words' size=1 value=<?php echo empty($_GET) ? 3 : $_GET['num_words']?>>
+		<input type='text' pattern=[1-9] name='num_words' size=1 value=<?php echo empty($_GET) ? 3 : $_GET['num_words']?>> <!-- value uses ternery operator-->
 		(1-9)<br>
 		<input type='checkbox' name='agreed'>
 		I am prepared to view the future and understand any and all associated risks.<br>
 		<input type='submit' value='View future!'>
 	</form>
 	
-	<?php if(isset($message))echo $message ?>
+	<?php if(isset($message))echo $message ?> <!-- message is output to be displayed -->
 	
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,31 +1,31 @@
 <?php
 $_Num_words_in_dict = 58110;
-if(isset($_GET['num_words']) and ! isset($_GET['agreed'])){
+if(isset($_GET['num_words']) and ! isset($_GET['agreed'])){ //isset($_GET['num_words']) checks whether form has been submitted
 	$message = '<p>Please consider whether you are prepared to know the future, then check the box above.</p>';
 }
 elseif(isset($_GET['num_words'])){
 	$num_words = $_GET['num_words'];
-	for($i=0; $i<$num_words; $i++){
+	for($i=0; $i<$num_words; $i++){ //loop to generate array of random indices into wordlist
 		$word_indices[$i]=rand(0,$_Num_words_in_dict-1);
 	}
-	sort($word_indices);
+	sort($word_indices);//sorts indices to allow more efficient search through dict
 	$dict=fopen('dict.txt', 'r'); //read only
-	$position_in_dict=0;
-	$next_array_index=0;
-	while($next_array_index<$num_words){
-		$word=substr(fgets($dict), 0, -1);
-		if($word_indices[$next_array_index]==$position_in_dict){
-			$future_words[$next_array_index]=$word;
+	$position_in_dict=0; //how many words have been read
+	$next_array_index=0; //how many words have been found
+	while($next_array_index<$num_words){ //search through dict until all words have been found
+		$word=substr(fgets($dict), 0, -1); //reads next line from dict and cuts off newline character
+		if($word_indices[$next_array_index]==$position_in_dict){ //checks if the next words corresponds to one of the desired indices
+			$future_words[$next_array_index]=$word; //generates an array of found words
 			$next_array_index++;
 		}
 		$position_in_dict++;
 	}
 	shuffle($future_words);
-	$future = $future_words[0];
+	$future = $future_words[0]; //these three lines concatenate the array of desired words
 	for($i=1; $i<$num_words; $i++){
 		$future.=' '.$future_words[$i]; //equivalent of future=future.[...]
 		
-	$message = '<br><h2>Behold your future:</h2> <h3>'.$future.'<h3>';
+	$message = '<br><h2>Behold your future:</h2> <h3>'.$future.'<h3>'; //html code for index.php
 		
 	}
 }
